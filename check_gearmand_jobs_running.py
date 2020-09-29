2551fad32458470da4489bc16a822b397d4b8b6a
Paulo Celestino <pauloriccardo@hotmail.com>
2020-09-29 16:18:40 -0300

    add verify interrupt

--- check_gearmand_jobs_running.py
+++ check_gearmand_jobs_running.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # ============================== SUMMARY =====================================
 # Author : Paulo Celestino
-# Date : 25/09/2020
-# Version : 1.1
+# Date : 28/09/2020
+# Version : 1.2
 # Licence : GPL
 # ===================== INFORMATION ABOUT THIS PLUGIN ========================
 #
@@ -29,14 +29,22 @@ def execute_command(commandstring):
         msg = "Exception calling command: '%s' , Exception: %s" % (commandstring,str(e))
         return(msg)
 
-total_jobs = 0
 result = execute_command(['gearman_top','-b']).stdout.readlines()
 
 for line in result:
     if('failed' in line):
-        total_jobs = 400
-        print("Critical - Gearman Total Jobs Running: %s | Total Jobs Running=%s;%s;%s;" % (total_jobs,total_jobs,warning,critical))
+        total_jobs = 200
+        print("Error - Gearman Total Jobs Running Return Failed  | Total Jobs Running=%d;%d;%d;" % (total_jobs,warning,critical))
         sys.exit(CRITICAL)
+
+    if('interrupt' in line):
+        total_jobs = 201
+        print("Error - Gearman Total Jobs Running Return Interrupt | Total Jobs Running=%d;%d;%d;" % (total_jobs,warning,critical))
+        sys.exit(CRITICAL)
+
+
+
+total_jobs = 0
 
 for i in range(4,len(result) - 1):
     cleanline = result[i].replace(' ','')
@@ -45,11 +53,11 @@ for i in range(4,len(result) - 1):
     total_jobs += int(jobs)
 
 if(total_jobs > critical):
-    print("Critical Gearman Total Jobs Running: %s | Total Jobs Running=%s;%s;%s;" % (total_jobs,total_jobs,warning,critical))
+    print("Critical Gearman Total Jobs Running: %s | Total Jobs Running=%d;%d;%d;" % (total_jobs,total_jobs,warning,critical))
     sys.exit(CRITICAL)
 elif(total_jobs > warning):
-    print("Warning - Gearman Total Jobs Running: %s | Total Jobs Running=%s;%s;%s;" % (total_jobs,total_jobs,warning,critical))
+    print("Warning - Gearman Total Jobs Running: %s | Total Jobs Running=%d;%d;%d;" % (total_jobs,total_jobs,warning,critical))
     sys.exit(WARNING)
 else:
-    print("OK - Gearman Total Jobs Running: %s | Total Jobs Running=%s;%s;%s;" % (total_jobs,total_jobs,warning,critical))
+    print("OK - Gearman Total Jobs Running: %s | Total Jobs Running=%d;%d;%d;" % (total_jobs,total_jobs,warning,critical))
     sys.exit(OK)
